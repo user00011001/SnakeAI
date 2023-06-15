bde25b225a7b33ea88820eb0a62d0c4c6f3e1a36
net1ife <127271713+net1ife@users.noreply.github.com>
2023-06-15 01:42:04 -0400

    Update snake.py

--- snake.py
+++ snake.py
@@ -14,11 +14,16 @@ GRID_SIZE = 20
 GRID_OFFSET = 2
 
 # AI parameters
-DISCOUNT_FACTOR = 0.8
+MODEL_PARAMS = [
+    {"name": "model1", "color": (255, 0, 0), "discount_factor": 0.8, "learning_rate": 0.01},
+    {"name": "model2", "color": (0, 255, 0), "discount_factor": 0.9, "learning_rate": 0.001},
+    {"name": "model", "color": (0, 0, 255), "discount_factor": 0.5, "learning_rate": 0.01},
+    # Add more models here
+]
+
 EPSILON_START = 1.0
 EPSILON_END = 0.01
 EPSILON_DECAY = 0.995
-LEARNING_RATE = 0.01
 BATCH_SIZE = 32
 REPLAY_MEMORY_SIZE = 5000
 
@@ -47,84 +52,88 @@ class DQN(nn.Module):
         x = torch.relu(self.fc1(x))
         return self.fc2(x)
 
-# Initialize the model
-model = DQN()
+# Initialize the models, criterions, optimizers, replay memories, and epsilon values
+models = []
+criterions = []
+optimizers = []
+replay_memories = []
+epsilons = []
 
-# Use Mean Squared Error loss for Q-Learning
-criterion = nn.MSELoss()
+for model_params in MODEL_PARAMS:
+    model = DQN()
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=model_params["learning_rate"])
+    replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
+    epsilon = EPSILON_START
 
-# Use Adam optimizer
-optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
-
-# Set the path for saving the model
-model_path = os.path.join(os.getcwd(), "model.pth")
-
-# Check if a saved model exists
-if os.path.isfile(model_path):
-    try:
-        model.load_state_dict(torch.load(model_path))
-        print("Loaded model parameters from:", model_path)
-    except (FileNotFoundError, RuntimeError):  # Added RuntimeError for size mismatch
-        print("No existing model parameters found or incompatible parameters. Starting training from scratch.")
-else:
-    print("No existing model parameters found. Starting training from scratch.")
-
-# Replay memory
-replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
-
-# AI game state
-snake = deque([(GRID_SIZE//2, GRID_SIZE//2)])
-directions = ["UP", "RIGHT", "DOWN", "LEFT"]
-direction = 0  # start moving up
-fruit = None
-
-# Epsilon value
-epsilon = EPSILON_START
+    models.append(model)
+    criterions.append(criterion)
+    optimizers.append(optimizer)
+    replay_memories.append(replay_memory)
+    epsilons.append(epsilon)
 
 # Function to spawn fruit
-def spawn_fruit():
-    global fruit
+def spawn_fruit(snake):
     while True:
         x, y = np.random.randint(GRID_SIZE), np.random.randint(GRID_SIZE)
         if (x, y) not in snake:
-            fruit = (x, y)
-            break
+            return (x, y)
 
-# Spawn a fruit at the beginning
-spawn_fruit()
+# AI game state
+snakes = []
+directions = ["UP", "RIGHT", "DOWN", "LEFT"]
+fruit = None  # Single shared apple position
 
-def draw_grid():
-    win.fill((0, 0, 0))
-    for x in range(GRID_SIZE):
-        for y in range(GRID_SIZE):
-            rect = pygame.Rect(x*(WINDOW_SIZE//GRID_SIZE), y*(WINDOW_SIZE//GRID_SIZE), WINDOW_SIZE//GRID_SIZE, WINDOW_SIZE//GRID_SIZE)
-            pygame.draw.rect(win, (0, 255, 0), rect, GRID_OFFSET)
-
-def draw_snake():
-    for position in snake:
-        x, y = position
-        rect = pygame.Rect(x*(WINDOW_SIZE//GRID_SIZE), y*(WINDOW_SIZE//GRID_SIZE), WINDOW_SIZE//GRID_SIZE, WINDOW_SIZE//GRID_SIZE)
-        pygame.draw.rect(win, (255, 255, 255), rect)
-
-def draw_fruit():
-    if fruit is not None:
-        x, y = fruit
-        rect = pygame.Rect(x*(WINDOW_SIZE//GRID_SIZE), y*(WINDOW_SIZE//GRID_SIZE), WINDOW_SIZE//GRID_SIZE, WINDOW_SIZE//GRID_SIZE)
-        pygame.draw.rect(win, (255, 0, 0), rect)
-
+# Function to reset the game
 def reset():
-    global snake, direction, fruit
-    snake = deque([(GRID_SIZE//2, GRID_SIZE//2)])
-    direction = 0
-    fruit = None
-    spawn_fruit()  
+    global snakes, directions, fruit
+    snakes = [deque([(GRID_SIZE//2, GRID_SIZE//2)]) for _ in range(len(MODEL_PARAMS))]
+    directions = [0] * len(MODEL_PARAMS)  # start moving up
+    fruit = spawn_fruit(snakes[0])  # Spawn a single shared fruit for all models
 
-def distance(x1, y1, x2, y2):
-    return np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+# Initialize models, criterions, optimizers, and replay memories
+models = []
+criterions = []
+optimizers = []
+replay_memories = []
 
-def step(action):
-    global direction, snake, fruit
-    head_x, head_y = snake[0]
+for model_params in MODEL_PARAMS:
+    model = DQN()
+    criterion = nn.MSELoss()
+    optimizer = optim.Adam(model.parameters(), lr=model_params["learning_rate"])
+    replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
+
+    models.append(model)
+    criterions.append(criterion)
+    optimizers.append(optimizer)
+    replay_memories.append(replay_memory)
+
+# Epsilon values for each model
+epsilons = [EPSILON_START] * len(MODEL_PARAMS)
+
+# Get the path for saving the models
+model_paths = [os.path.join(os.getcwd(), model_params["name"] + ".pth") for model_params in MODEL_PARAMS]
+
+# Load the model parameters if the files exist
+for i, model_path in enumerate(model_paths):
+    if os.path.isfile(model_path):
+        try:
+            models[i].load_state_dict(torch.load(model_path))
+            print("Loaded model parameters from:", model_path)
+        except (FileNotFoundError, RuntimeError):
+            print("No existing model parameters found or incompatible parameters. Starting training from scratch.")
+    else:
+        print("No existing model parameters found. Starting training from scratch.")
+
+# Update the epsilon value for each model
+def update_epsilons():
+    global epsilons
+    epsilons = [max(epsilon * EPSILON_DECAY, EPSILON_END) for epsilon in epsilons]
+
+# Step function for each model
+def step(action, model_index):
+    global directions, snakes, fruit
+    head_x, head_y = snakes[model_index][0]
     if action == 0:  # UP
         head_y -= 1
     elif action == 1:  # RIGHT
@@ -135,21 +144,23 @@ def step(action):
         head_x -= 1
     head_x %= GRID_SIZE
     head_y %= GRID_SIZE
-    if (head_x, head_y) in snake:
-        snake = deque([(head_x, head_y)])  # Reset snake's size to 1
-        return -10  # hit self
-    snake.appendleft((head_x, head_y))
+    if (head_x, head_y) in snakes[model_index] or any((head_x, head_y) in s for i, s in enumerate(snakes) if i != model_index):
+        # Reset the snake's size to its initial state
+        snakes[model_index] = deque([(GRID_SIZE//2, GRID_SIZE//2)])
+        return -10  # hit self or other model
+    snakes[model_index].appendleft((head_x, head_y))
     if (head_x, head_y) == fruit:
         fruit = None  # remove the eaten fruit
-        spawn_fruit()  # spawn a new fruit
+        fruit = spawn_fruit(snakes[model_index])  # spawn a new fruit
         return 10  # got fruit
     else:
-        snake.pop()  # didn't get fruit, remove tail
+        snakes[model_index].pop()  # didn't get fruit, remove tail
         if fruit is None:
-            spawn_fruit()
+            fruit = spawn_fruit(snakes[model_index])
         return -1 + 1 / distance(head_x, head_y, fruit[0], fruit[1])  # normal step + smaller reward for getting closer
 
-def update_model(batch):
+# Update the model for each model
+def update_models(batch, model_index):
     states, actions, rewards, new_states, dones = zip(*batch)
 
     states = torch.from_numpy(np.array(states)).float()  # Do not add an extra dimension
@@ -158,13 +169,16 @@ def update_model(batch):
     new_states = torch.from_numpy(np.array(new_states)).float()  # Do not add an extra dimension
     dones = torch.tensor(np.array(dones, dtype=bool), dtype=torch.bool)
 
+    model = models[model_index]
+    criterion = criterions[model_index]
+    optimizer = optimizers[model_index]
 
     q_values = model(states)
     next_q_values = model(new_states)
 
     q_values = q_values.gather(1, actions.unsqueeze(1)).squeeze()
     next_q_value = next_q_values.max(1)[0]
-    expected_q_values = rewards + DISCOUNT_FACTOR * next_q_value * (~dones)
+    expected_q_values = rewards + model_params["discount_factor"] * next_q_value * (~dones)
 
     loss = criterion(q_values, expected_q_values.detach())
 
@@ -172,75 +186,121 @@ def update_model(batch):
     loss.backward()
     optimizer.step()
 
+# Update the epsilon for each model
+def update_epsilon(model_index):
+    epsilons[model_index] = max(epsilons[model_index] * EPSILON_DECAY, EPSILON_END)
 
-def update_epsilon():
-    global epsilon
-    epsilon = max(epsilon * EPSILON_DECAY, EPSILON_END)
+# Function to calculate the distance between two points
+def distance(x1, y1, x2, y2):
+    return np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
 
-# Load the model parameters if the file exists
-model_path = "model.pth"
-try:
-    model.load_state_dict(torch.load(model_path))
-    print("Loaded model parameters from:", os.path.abspath(model_path))
-except (FileNotFoundError, RuntimeError):  # Added RuntimeError for size mismatch
-    print("No existing model parameters found or incompatible parameters. Starting training from scratch.")
+# Function to draw the grid
+def draw_grid():
+    win.fill((0, 0, 0))
+    for x in range(GRID_SIZE):
+        for y in range(GRID_SIZE):
+            rect = pygame.Rect(x*(WINDOW_SIZE//GRID_SIZE), y*(WINDOW_SIZE//GRID_SIZE), WINDOW_SIZE//GRID_SIZE, WINDOW_SIZE//GRID_SIZE)
+            pygame.draw.rect(win, (0, 255, 0), rect, GRID_OFFSET)
 
-print("Current working directory:", os.getcwd())
+# Function to draw the snake for each model
+def draw_snakes():
+    for i, snake in enumerate(snakes):
+        color = MODEL_PARAMS[i]["color"]
+        for j, position in enumerate(snake):
+            x, y = position
+            rect = pygame.Rect(x*(WINDOW_SIZE//GRID_SIZE), y*(WINDOW_SIZE//GRID_SIZE), WINDOW_SIZE//GRID_SIZE, WINDOW_SIZE//GRID_SIZE)
+            pygame.draw.rect(win, color, rect)
+            # Draw the head larger
+            if j == 0:
+                rect = pygame.Rect(x*(WINDOW_SIZE//GRID_SIZE)+GRID_OFFSET, y*(WINDOW_SIZE//GRID_SIZE)+GRID_OFFSET, WINDOW_SIZE//GRID_SIZE-2*GRID_OFFSET, WINDOW_SIZE//GRID_SIZE-2*GRID_OFFSET)
+                pygame.draw.rect(win, color, rect)
+
+
+# Function to draw the fruits for each model
+def draw_fruits():
+    if fruit is not None:
+        for i in range(len(MODEL_PARAMS)):
+            color = MODEL_PARAMS[i]["color"]
+            x, y = fruit
+            rect = pygame.Rect(x*(WINDOW_SIZE//GRID_SIZE), y*(WINDOW_SIZE//GRID_SIZE), WINDOW_SIZE//GRID_SIZE, WINDOW_SIZE//GRID_SIZE)
+            pygame.draw.rect(win, color, rect)
+
+# Reset the game
+reset()
 
 # Game loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             # Save the model parameters before exiting
-            torch.save(model.state_dict(), model_path)
-            print("Saved model parameters to:", os.path.abspath(model_path))
+            for i, model_path in enumerate(model_paths):
+                torch.save(models[i].state_dict(), model_path)
+                print("Saved model parameters to:", os.path.abspath(model_path))
             pygame.quit()
             sys.exit()
 
-    # Get state
-    state = np.zeros((2, GRID_SIZE, GRID_SIZE))  # Change from 1 to 2
-    head_x, head_y = snake[0]
-    state[0][head_y][head_x] = 1  # mark head position
-    for i, (x, y) in enumerate(snake):
-        if i > 0:
-            state[0][y][x] = -1  # mark snake body
-    state[1] = distance(head_x, head_y, fruit[0], fruit[1])  # Add distance to fruit as a feature
+    # Get states for each model
+    states = []
+    for i in range(len(models)):
+        state = np.zeros((2, GRID_SIZE, GRID_SIZE))  # Change from 1 to 2
+        head_x, head_y = snakes[i][0]
+        state[0][head_y][head_x] = 1  # mark head position
+        for j, (x, y) in enumerate(snakes[i]):
+            if j > 0:
+                state[0][y][x] = -1  # mark snake body
+        if fruit is not None:
+            state[1] = distance(head_x, head_y, fruit[0], fruit[1])  # Add distance to fruit as a feature
+        states.append(state)
 
-    # AI decision
-    q_values = model(torch.from_numpy(state).float().unsqueeze(0))
-    if random.random() < epsilon:
-        action = random.randint(0, 3)
-    else:
-        _, action = q_values.max(1)
+    # AI decision for each model
+    actions = []
+    for i, model in enumerate(models):
+        q_values = model(torch.from_numpy(states[i]).float().unsqueeze(0))
+        if random.random() < epsilons[i]:
+            action = random.randint(0, 3)
+        else:
+            _, action = q_values.max(1)
+        actions.append(action)
 
+    # Perform step for each model
+    rewards = []
+    new_states = []
+    for i, action in enumerate(actions):
+        reward = step(action, i)
+        rewards.append(reward)
 
-    # Perform step
-    reward = step(action)
+        # Get new state
+        new_state = np.zeros((2, GRID_SIZE, GRID_SIZE))  # Change from 1 to 2
+        head_x, head_y = snakes[i][0]
+        new_state[0][head_y][head_x] = 1  # mark head position
+        for j, (x, y) in enumerate(snakes[i]):
+            if j > 0:
+                new_state[0][y][x] = -1  # mark snake body
+        if fruit is not None:
+            new_state[1] = distance(head_x, head_y, fruit[0], fruit[1])  # Add distance to fruit as a feature
+        new_states.append(new_state)
 
-    # Get new state
-    new_state = np.zeros((2, GRID_SIZE, GRID_SIZE))  # Change from 1 to 2
-    head_x, head_y = snake[0]
-    new_state[0][head_y][head_x] = 1  # mark head position
-    for i, (x, y) in enumerate(snake):
-        if i > 0:
-            new_state[0][y][x] = -1  # mark snake body
-    new_state[1] = distance(head_x, head_y, fruit[0], fruit[1])  # Add distance to fruit as a feature
+        # Check for collisions
+        if reward == -10:
+            # Reset the size of the snake to its initial state
+            snakes[i] = deque([(GRID_SIZE//2, GRID_SIZE//2)])
 
-    # Add to replay memory
-    replay_memory.append((state, action, reward, new_state, reward < 0))
+    # Add to replay memory for each model
+    for i in range(len(models)):
+        replay_memories[i].append((states[i], actions[i], rewards[i], new_states[i], rewards[i] < 0))
 
-    # Update model
-    if len(replay_memory) >= BATCH_SIZE:
-        batch = random.sample(replay_memory, BATCH_SIZE)
-        update_model(batch)
+        # Update model for each model
+        if len(replay_memories[i]) >= BATCH_SIZE:
+            batch = random.sample(replay_memories[i], BATCH_SIZE)
+            update_models(batch, i)
 
-    # Update epsilon
-    update_epsilon()
+        # Update epsilon for each model
+        update_epsilon(i)
 
     # Draw everything
     draw_grid()
-    draw_snake()
-    draw_fruit()
+    draw_snakes()
+    draw_fruits()
 
     # Update the window
     pygame.display.update()
