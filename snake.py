ee06821fcd4e3a5edeefa32ba80c1c70d6f065f1
net1ife <127271713+net1ife@users.noreply.github.com>
2023-06-15 01:06:07 -0400

    Update snake.py

--- snake.py
+++ snake.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 import torch.optim as optim
 import random
 from collections import deque
+import os
 
 # Pygame parameters
 WINDOW_SIZE = 600
@@ -34,7 +35,7 @@ clock = pygame.time.Clock()
 class DQN(nn.Module):
     def __init__(self):
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, kernel_size=3, stride=1)
+        self.conv1 = nn.Conv2d(2, 16, kernel_size=3, stride=1)  # Change the input channel to 2
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1)
         self.fc1 = nn.Linear(32 * 16 * 16, 256)
         self.fc2 = nn.Linear(256, 4)  # output for each action
@@ -55,6 +56,22 @@ criterion = nn.MSELoss()
 # Use Adam optimizer
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
+# Set the path for saving the model
+model_path = os.path.join(os.getcwd(), "model.pth")
+
+# Check if a saved model exists
+if os.path.isfile(model_path):
+    try:
+        model.load_state_dict(torch.load(model_path))
+        print("Loaded model parameters from:", model_path)
+    except (FileNotFoundError, RuntimeError):  # Added RuntimeError for size mismatch
+        print("No existing model parameters found or incompatible parameters. Starting training from scratch.")
+else:
+    print("No existing model parameters found. Starting training from scratch.")
+
+# Replay memory
+replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
+
 # AI game state
 snake = deque([(GRID_SIZE//2, GRID_SIZE//2)])
 directions = ["UP", "RIGHT", "DOWN", "LEFT"]
@@ -64,8 +81,17 @@ fruit = None
 # Epsilon value
 epsilon = EPSILON_START
 
-# Replay memory
-replay_memory = deque(maxlen=REPLAY_MEMORY_SIZE)
+# Function to spawn fruit
+def spawn_fruit():
+    global fruit
+    while True:
+        x, y = np.random.randint(GRID_SIZE), np.random.randint(GRID_SIZE)
+        if (x, y) not in snake:
+            fruit = (x, y)
+            break
+
+# Spawn a fruit at the beginning
+spawn_fruit()
 
 def draw_grid():
     win.fill((0, 0, 0))
@@ -91,14 +117,10 @@ def reset():
     snake = deque([(GRID_SIZE//2, GRID_SIZE//2)])
     direction = 0
     fruit = None
+    spawn_fruit()  
 
-def spawn_fruit():
-    global fruit
-    while True:
-        x, y = np.random.randint(GRID_SIZE), np.random.randint(GRID_SIZE)
-        if (x, y) not in snake:
-            fruit = (x, y)
-            break
+def distance(x1, y1, x2, y2):
+    return np.sqrt((x1 - x2)**2 + (y1 - y2)**2)
 
 def step(action):
     global direction, snake, fruit
@@ -116,23 +138,25 @@ def step(action):
     if (head_x, head_y) in snake:
         return -10  # hit self
     snake.appendleft((head_x, head_y))
-    if fruit is None or (head_x, head_y) != fruit:
+    if (head_x, head_y) == fruit:
+        fruit = None  # remove the eaten fruit
+        spawn_fruit()  # spawn a new fruit
+        return 10  # got fruit
+    else:
         snake.pop()  # didn't get fruit, remove tail
         if fruit is None:
             spawn_fruit()
-        return -1  # normal step
-    else:
-        fruit = None  # remove the eaten fruit
-        return 10  # got fruit
+        return -1 + 1 / distance(head_x, head_y, fruit[0], fruit[1])  # normal step + smaller reward for getting closer
 
 def update_model(batch):
     states, actions, rewards, new_states, dones = zip(*batch)
 
-    states = torch.from_numpy(np.array(states)).float().unsqueeze(1)  # Add a channel dimension
+    states = torch.from_numpy(np.array(states)).float()  # Do not add an extra dimension
     actions = torch.tensor(actions)
     rewards = torch.tensor(rewards, dtype=torch.float)
-    new_states = torch.from_numpy(np.array(new_states)).float().unsqueeze(1)  # Add a channel dimension
-    dones = torch.tensor(dones, dtype=torch.bool)
+    new_states = torch.from_numpy(np.array(new_states)).float()  # Do not add an extra dimension
+    dones = torch.tensor(np.array(dones, dtype=bool), dtype=torch.bool)
+
 
     q_values = model(states)
     next_q_values = model(new_states)
@@ -147,73 +171,78 @@ def update_model(batch):
     loss.backward()
     optimizer.step()
 
+
 def update_epsilon():
     global epsilon
     epsilon = max(epsilon * EPSILON_DECAY, EPSILON_END)
 
 # Load the model parameters if the file exists
+model_path = "model.pth"
 try:
-    model.load_state_dict(torch.load("model.pth"))
-    print("Loaded model parameters from 'model.pth'")
-except FileNotFoundError:
-    print("No existing model parameters found. Starting training from scratch.")
+    model.load_state_dict(torch.load(model_path))
+    print("Loaded model parameters from:", os.path.abspath(model_path))
+except (FileNotFoundError, RuntimeError):  # Added RuntimeError for size mismatch
+    print("No existing model parameters found or incompatible parameters. Starting training from scratch.")
+
+print("Current working directory:", os.getcwd())
 
 # Game loop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             # Save the model parameters before exiting
-            torch.save(model.state_dict(), "model.pth")
-            print("Saved model parameters to 'model.pth'")
+            torch.save(model.state_dict(), model_path)
+            print("Saved model parameters to:", os.path.abspath(model_path))
             pygame.quit()
             sys.exit()
 
     # Get state
-    state = np.zeros((GRID_SIZE, GRID_SIZE))
+    state = np.zeros((2, GRID_SIZE, GRID_SIZE))  # Change from 1 to 2
     head_x, head_y = snake[0]
-    state[head_y][head_x] = 1  # mark head position
+    state[0][head_y][head_x] = 1  # mark head position
     for i, (x, y) in enumerate(snake):
         if i > 0:
-            state[y][x] = -1  # mark snake body
+            state[0][y][x] = -1  # mark snake body
+    state[1] = distance(head_x, head_y, fruit[0], fruit[1])  # Add distance to fruit as a feature
 
-    # Select action
+    # AI decision
+    q_values = model(torch.from_numpy(state).float().unsqueeze(0))
     if random.random() < epsilon:
-        action = random.randint(0, 3)  # random action
+        action = random.randint(0, 3)
     else:
-        with torch.no_grad():
-            state_tensor = torch.from_numpy(state).float().unsqueeze(0)
-            action = torch.argmax(model(state_tensor)).item()  # best action
+        _, action = q_values.max(1)
 
-    # Perform action
+
+    # Perform step
     reward = step(action)
-    done = reward == -10
 
     # Get new state
-    new_state = np.zeros((GRID_SIZE, GRID_SIZE))
+    new_state = np.zeros((2, GRID_SIZE, GRID_SIZE))  # Change from 1 to 2
     head_x, head_y = snake[0]
-    new_state[head_y][head_x] = 1  # mark head position
+    new_state[0][head_y][head_x] = 1  # mark head position
     for i, (x, y) in enumerate(snake):
         if i > 0:
-            new_state[y][x] = -1  # mark snake body
+            new_state[0][y][x] = -1  # mark snake body
+    new_state[1] = distance(head_x, head_y, fruit[0], fruit[1])  # Add distance to fruit as a feature
 
-    # Store experience in replay memory
-    replay_memory.append((state, action, reward, new_state, done))
+    # Add to replay memory
+    replay_memory.append((state, action, reward, new_state, reward < 0))
 
-    # Sample random batch from replay memory
+    # Update model
     if len(replay_memory) >= BATCH_SIZE:
         batch = random.sample(replay_memory, BATCH_SIZE)
         update_model(batch)
 
-    # Decay epsilon
+    # Update epsilon
     update_epsilon()
 
-    if done:
-        reset()
-
-    # Drawing
+    # Draw everything
     draw_grid()
     draw_snake()
     draw_fruit()
 
+    # Update the window
     pygame.display.update()
+
+    # Control the frame rate
     clock.tick(10)
